c1b7380f16bf2e670a4234f843ac8736e6698729
Bogdan Kiptenko <bogdankiptenko@gmail.com>
2020-04-20 14:04:13 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8Task3
+{
+    class Program: Sphere
+    {
+        delegate double Operations(double r);
+        static void Main(string[] args)
+        {
+            Sphere s = new Sphere();
+            Operations op1 = s.CircleLenght;
+            Console.WriteLine("Circle lenght: " + op1(3));
+            op1 = s.CircleArea;
+            Console.WriteLine("Circle area: " + op1(3));
+            op1 = s.SphereVolume;
+            Console.WriteLine("Sphere volume: " + op1(3));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sphere.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8Task3
+{
+    public class Sphere
+    {
+        public Sphere()
+        { }
+        public double CircleLenght(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        public double CircleArea(double r)
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
+        public double SphereVolume(double r)
+        {
+            return (4/3) * Math.PI * Math.Pow(r, 3);
+        }
+    }
+}
